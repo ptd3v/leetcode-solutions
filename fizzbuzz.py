662b3fe3eef81900da67d6afd25582ef636babb6
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-08-08 17:15:13 +0100

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,3 +2,10 @@
 #Overview: Print a range of numbers. If a number is divisible by x, print A. If a number if divisible by y, print B. If a number if divisible by x and y, print AB.
 #Purpose: To asses a candidate's basic programming skills and problem-solving abilities.
 #Skills Required: Problem Solving, Logic, Modulo, Loops, Conditional Statements.
+
+#Create a loop for numbers between 0 and 20.
+for number in range (0,20):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  else
+    print(number)
