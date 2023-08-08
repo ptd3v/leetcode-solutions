413f0d329663cd083d2056d27db597d2411ef1e4
Paul T <102877038+ptd3v@users.noreply.github.com>
2023-08-08 17:11:41 +0100

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,4 @@
 #Interview Essentials 1: FizzBuzz
 #Overview: Print a range of numbers. If a number is divisible by x, print A. If a number if divisible by y, print B. If a number if divisible by x and y, print AB.
 #Purpose: To asses a candidate's basic programming skills and problem-solving abilities.
+#Skills Required: Problem Solving, Logic, Modulo, Loops, Conditional Statements.
