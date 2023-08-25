6a0be04ed0e5a1d92998ab0af63c4b7ab0adfe6c
djtbcode <129532735+djtbcode@users.noreply.github.com>
2023-08-25 22:20:39 +0100

    trial 1

---
+++ fizzbuzz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int range = 21
+    for (int i = 0; i < range; i++)
+    {
+        if (range[i] % 3 == 0 && range[i] % 5 == 0)
+        printf("fizzbuzz");
+        
+    }
+}
