0bbefd762437d3c83d23c8e94943e7f96c794dc9
djtbcode <129532735+djtbcode@users.noreply.github.com>
2023-08-26 17:46:24 +0100

    Completed #challange

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -2,29 +2,32 @@
 
 int main (void)
 {
-    int range = 21;
-    int count = 0;
-    for (int i = 0; i < range; i++)
+    int range = 1000; //Variable to control range of Fizzbuzz
+    int count = 0; //Challange Counter Variable 
+    for (int i = 1; i < range; i++)
     {
-        if ( i % 3 = 0 && i % 5 = 0)
+        if ( i % 3 == 0 && i % 5 == 0)
         {
             printf("fizzbuzz\n");
-            count += i;
+            count += i; // Add the value of I if divisable by 3 AND 5 to count 
         }
-        else if (i % 3 = 0)
+        else if (i % 3 == 0)
         {
             printf("Fizz\n");
-            
+            count += i;// Add the value of I if divisable by 3 to count 
+
         }
-        else if (i % 5 = 0)
+        else if (i % 5 == 0)
         {
             printf("Buzz\n");
+            count += i;// Add the value of I if divisable by 5 to count 
         }
-        else 
+        else
         {
-            printf("d%\n");
+            printf("%i\n", i);
         }
     }
 
-    printf("Fizzbuzz Count = %d", count);
+    printf("Fizzbuzz Count = %i\n", count);//Display count value tested with given example range 10, count = 23. 
+    //value when range = 1000, count = 233168
 }
