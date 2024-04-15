819c49db41d08559fd107de4d7f8b6b835d51d9e
ptd3v <tovey@live.co.uk>
2024-04-15 21:36:12 +0100

    Solution + Slicing Explanation

---
+++ reversing-strings.py
@@ -0,0 +1,16 @@
+# Reversed Strings - CodeWars (8th Kyu)
+# Complete the solution so that it reverses the string passed into it.
+# 'world'  =>  'dlrow', 'word'   =>  'drow'
+
+# Solution 1 - String provided
+def solution(string):
+    return string[::-1]
+
+# Solution 2 - Custom Strings
+my_string = "Hello, world!"
+reversed_string = my_string[::-1]
+print(reversed_string)
+
+# Both solutions use 'slicing', in the following format: [start-position:end-poition:step-size]
+# For example, [1:10:2] = From position 1 to 10, show every second letter.
+# In this case, [::-1] = For all position, print 1 step backwards.
