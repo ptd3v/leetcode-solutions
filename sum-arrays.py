ddbc31d105d9381ef96ddb331a54e7cfc4efd142
ptd3v <tovey@live.co.uk>
2024-04-16 15:06:42 +0100

    Sum Arrays - CodeWars

---
+++ sum-arrays.py
@@ -0,0 +1,25 @@
+# Sum Arrays - CodeWars (8th Kyu)
+# Write a function that takes an array of numbers and returns the sum of the numbers.
+# The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+# Example Data
+numbers = [1,2,3,6,78,8] # Single data type only
+
+# Solution 1 (Sum Function)
+def sum_array(numbers):
+    if not numbers: # Error handling for empty array
+        return 0
+    return sum(numbers)
+
+# Solution 2 (For Loop)
+def sum_loop(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+# Outputs
+print(numbers) # Outputs the array contents
+print(sum_array) # Outputs the memory location of the array
+print(sum_array(numbers)) # Outputs the sum total of the array, solution 1.
+print(sum_loop(numbers)) # Outputs the sum total of the array, solution 2.
