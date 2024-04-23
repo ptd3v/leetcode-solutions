995b007cfe922d1685eae9e77551c32f9be09783
ptd3v <tovey@live.co.uk>
2024-04-23 14:45:25 +0100

    return s[mid - 1: mid + 1] if len(s) % 2 == 0 else s[mid]

--- get-the-middle-character.py
+++ get-the-middle-character.py
@@ -3,9 +3,19 @@
 # If the word's length is even, return the middle 2 characters.
 
 # Solution One
-
+def get_middle(s):
+    mid = len(s) // 2
+    if len(s) % 2 == 0:
+        return s[mid -1: mid +1]
+    else:
+        return s[mid]
 
 # Solution Two: Advanced
-
+def get_middle(s):
+    mid = len(s) // 2
+    return s[mid - 1: mid + 1] if len(s) % 2 == 0 else s[mid]
 
 # Solution Three: Community
+def get_middle(s):
+    i = (len(s) - 1) // 2
+    return s[i:-i] or s
