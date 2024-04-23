b7762aa98d3851f079e8486b5aba21a59646c85c
ptd3v <tovey@live.co.uk>
2024-04-23 13:23:22 +0100

    Get the Middle Character (7th Kyu) Start

---
+++ get-the-middle-character.py
@@ -0,0 +1,11 @@
+# Get the Middle Character (7th Kyu)
+# Your job is to return the middle character of the word.
+# If the word's length is even, return the middle 2 characters.
+
+# Solution One
+
+
+# Solution Two: Advanced
+
+
+# Solution Three: Community
