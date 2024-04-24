cebb9d1a9679217996b395daf27ba3db097eb126
ptd3v <tovey@live.co.uk>
2024-04-24 15:27:32 +0100

    (c.upper() + c.lower() * i for i, c in enumerate(s))

--- mumbling.py
+++ mumbling.py
@@ -1,8 +1,14 @@
 # Mumbling (7th Kyu)
-# 
+# accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+# accum("cwAt") -> "C-Ww-Aaa-Tttt"
 
 # Solution One
-
-# Solution Two: Advanced
+def accum(st):
+    newlist = [] # Open a new list.
+    for indx, st in enumerate(st, 1): # Start index at 1, instead of 0.
+        newlist.append((st * indx).title()) # Title for first capital. st * index value. E.g C * 3.
+    return '-'.join(newlist)
 
 # Solution Three: Community
+def accum(s):
+    return '-'.join(c.upper() + c.lower() * i for i, c in enumerate(s)) # Way too many smart cookies on here.
