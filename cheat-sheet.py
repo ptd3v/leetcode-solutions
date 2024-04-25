34a5f2ac627bdc9e76c6b369f9fe9f1e96329a45
ptd3v <tovey@live.co.uk>
2024-04-25 19:21:26 +0100

    print(tuple[::-1])      # Start:End:Step

---
+++ cheat-sheet.py
@@ -0,0 +1,27 @@
+list =  [1, 2, 3, 4]    # Ordered. Changeable. 0 Index.
+tuple = (1, 2, 3, 4)    # Ordered. Unchangeable. 0 Index. Faster.
+set =   {1, 2, 3, 4}    # Unordered. Unchangeable (Add/ Remove ONLY). No index. No Duplicates.
+#set()                  # Empty Set
+dict =  {'A':1,'B':2}   # Key: Value
+
+print(list[0])          # List, Tuple.
+print(tuple[::-1])      # Start:End:Step
+print(1 in set)         # True
+print(len(set))         # Length of Set
+print(list.index(1))    # Return index location
+print(list.count(2))    # Total 2's
+
+
+list[0] = 1             # Assign new value
+list.append(5)
+list.remove(5)
+list.insert(0, 0)       # Insert 0 at [0]
+list.sort()
+list.reverse()
+
+set.add(5)
+set.remove(5)
+
+
+for x in list:
+    print(x)
