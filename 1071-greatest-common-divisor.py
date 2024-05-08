2dd27386cc9fd3fbf746f50d9b37a9680d32795c
ptd3v <tovey@live.co.uk>
2024-05-08 20:35:30 +0100

    return str1[:gcd(len(str1), len(str2))]

--- 1071-greatest-common-divisor.py
+++ 1071-greatest-common-divisor.py
@@ -6,17 +6,15 @@
 # My Solution:
 class Solution:
     def gcdOfStrings(self, str1, str2):
-        # Check if the concatenation of the two strings in different orders results in the same string.
         if str1 + str2 != str2 + str1:
-            return '' # If not, there is no common divisor string, so return an empty string.    
-          
-        len_gcd = self.gcd(len(str1), len(str2)) # Find the gcd of the lengths of the two strings - ???       
-        return str1[:len_gcd] # Return the substring from 0 to len_gcd from the first string, which is the greatest common divisor string.
-    
-    def gcd(self, a, b): # Helper function to calculate the greatest common divisor
-        while b:
-            a, b = b, a % b
-        return a
+            return ""
+        
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+            
+        return str1[:gcd(len(str1), len(str2))]
 
 # Community Solution
 class Solution(object):
@@ -34,3 +32,4 @@ class Solution(object):
 # If the lengths of str1 and str2 are equal, and the concatenated strings are equal, then str1 (or str2) itself is the greatest common divisor, and we return str1 (or str2).
 # If the length of str1 is greater than the length of str2, it means that str1 contains str2 as a prefix. In this case, we recurse with the substring of str1 after removing (slicing) the prefix that matches str2.
 # If the length of str2 is greater than the length of str1, it means that str2 contains str1 as a prefix. In this case, we recurse with the substring of str2 after removing (slicing) the prefix that matches str1.
+
