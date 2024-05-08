bed0ba4541d2caa09c8bcb0da537c0cecdc5f8ce
ptd3v <tovey@live.co.uk>
2024-05-08 14:43:50 +0100

    while i < len(word1) or i < len(word2):

---
+++ 1071-greatest-common-divisor.py
@@ -0,0 +1,6 @@
+# Greatest Common Divisor of Strings (1071)
+# You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1.
+
+# Functions Required: len(), .join(), append(), max(), while loop.
+
+# My Solution:
