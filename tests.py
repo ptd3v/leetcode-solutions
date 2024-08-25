75b61c7b055c84303c5b36fa8f9ce473681e889a
ptd3v <102877038+ptd3v@users.noreply.github.com>
2024-08-25 18:00:53 +0100

    def condStat(balance):     if balance <= 50:

--- tests.py
+++ tests.py
@@ -3,3 +3,17 @@ def defaultPara(name="default"):    # Default name will be default
 
 defaultPara()           # Will print default
 defaultPara("Steve")    # Will print Steve
+
+# Condition inside if statements do not have their own scope?
+if True:                # Will always be True
+    message = "Hello, True"
+
+print(message)          # Can execute code in the if statement
+
+def condStat(balance):
+    if balance <= 50:
+        message2 = "Balance is low"
+    print(message2)
+
+condStat(50)
+#print(message2)     
